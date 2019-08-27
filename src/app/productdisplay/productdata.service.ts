8fda12997fe61942462717582589c0f10d625dee
User1 <user1@Skycliffitdomain.com>
2019-08-27 18:04:44 +0530

    added url in environments

--- src/app/productdisplay/productdata.service.ts
+++ src/app/productdisplay/productdata.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { endpoints } from "../../environments/environment";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { endpoints } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductdataService {
-  url: string =endpoints.url+ "products/";
+  url: string =endpoints.url+ 'products/';
   constructor(private _http: HttpClient) {}
 
   getAllProducts() {
@@ -17,16 +17,16 @@ export class ProductdataService {
   }
   addProduct(item) {
     let body = JSON.stringify(item);
-    let head = new HttpHeaders().set("Content-Type", "application/json");
+    let head = new HttpHeaders().set('Content-Type', 'application/json');
     return this._http.post(this.url, body, { headers: head });
   }
   editProduct(pro_id,item) {
     let body = JSON.stringify(item);
-    let head = new HttpHeaders().set("Content-Type", "application/json");
+    let head = new HttpHeaders().set('Content-Type', 'application/json');
     return this._http.put(this.url+pro_id, body, { headers: head });
   }
   deleteProduct(pro_id) {
-    let head = new HttpHeaders().set("Content-Type", "application/json");
+    let head = new HttpHeaders().set('Content-Type', 'application/json');
     return this._http.delete(this.url + pro_id, { headers: head });
   }
 }
