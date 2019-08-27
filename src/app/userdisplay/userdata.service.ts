36eed444365ec4f88c60e4a627970d74f832e56c
User1 <user1@Skycliffitdomain.com>
2019-08-27 17:58:06 +0530

    changed

--- src/app/userdisplay/userdata.service.ts
+++ src/app/userdisplay/userdata.service.ts
@@ -18,3 +18,30 @@ url:"https://nodejsapidemo.herokuapp.com/users/";
     // );
   }
 }
+currentUser;
+  redirectURL: string;
+  login(user_email: string, user_password: string) {
+    if (user_email == "admin" && user_password == "1234") {
+      this.currentUser = {
+        user_email: user_email,
+        password: user_password,
+        isAdmin: true
+      };
+      return;
+    }
+    this.currentUser = {
+      user_email: user_email,
+      password: user_password,
+      isAdmin: false
+    };
+  }
+  logout() {
+    this.currentUser = null;
+    this.redirectURL = "";
+    this._router.navigate([""]);
+  }
+  get isLoggedIn(): boolean {
+    return !!this.currentUser;
+  }
+}
+
