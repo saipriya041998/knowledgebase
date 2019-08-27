571dff73b446c63d58ba10fa7659572992933638
User1 <user1@Skycliffitdomain.com>
2019-08-27 17:13:16 +0530

    added signupreactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 <<<<<<< HEAD
 import { UserdisplayComponent } from './userdisplay/userdisplay.component';
+import { SignupreactiveComponent } from './userdisplay/signupreactive/signupreactive.component';
 =======
 import { TaskdisplayComponent } from './taskdisplay/taskdisplay.component';
 >>>>>>> 5cc9313251bdfb2a0eb3f7685ac592ee7fe3cccf
@@ -13,14 +14,16 @@ import { TaskdisplayComponent } from './taskdisplay/taskdisplay.component';
   declarations: [
     AppComponent,
 <<<<<<< HEAD
-    UserdisplayComponent
+    UserdisplayComponent,
+SignupreactiveComponent
 =======
     TaskdisplayComponent
 >>>>>>> 5cc9313251bdfb2a0eb3f7685ac592ee7fe3cccf
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/userdisplay/userdata.service.ts
+++ src/app/userdisplay/userdata.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +9,12 @@ url:"https://nodejsapidemo.herokuapp.com/users/";
   constructor(private _http:HttpClient) { }
   getAllUsers(){
     return this._http.get(this.url);
+  }
+
+  addUser(newuser) {
+    let head = new HttpHeaders().set("Content-Type", "application/json");
+    let body = JSON.stringify(newuser);
+    return this._http.post(this.url, body,{headers:head});
+    );
   }
 }
