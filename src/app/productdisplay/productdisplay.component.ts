352f1ca5e3a951158a3718614949792dc36dd36e
User1 <user1@Skycliffitdomain.com>
2019-08-27 18:27:49 +0530

    editprouduct,productdisply

--- src/app/productdisplay/productdisplay.component.ts
+++ src/app/productdisplay/productdisplay.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./productdisplay.component.scss']
 })
 export class ProductdisplayComponent implements OnInit {
-
-  constructor() { }
+  arr: Product[] = [];
+  name: string = "watch";
+  loading:boolean=true;
+  constructor(private _data: ProductdataService,private _router:Router) {}
 
   ngOnInit() {
+    this.loading=true;
+    this._data.getAllProducts().subscribe(
+      (data: Product[]) => {
+        this.arr = data;
+        this.loading=false;
+      },
+      function(error) {
+        alert(error);
+      },
+      function() {}
+    );
   }
-
+  onProductDelete(item: Product) {
+    this._data.deleteProduct(item.pro_id).subscribe((data: any) => {
+      this.arr.splice(this.arr.indexOf(item), 1);
+    });
+  }
+  onProductEdit(item:Product){
+    this._router.navigate(['/product/editproduct',item.pro_id]);
+  }
+  onSideBarClick(value) {
+    if (value != "") {
+      this.arr = this.arr.filter(x => x.pro_name.indexOf(value) != -1);
+    } else {
+      this._data.getAllProducts().subscribe(
+        (data: Product[]) => {
+          this.arr = data;
+        },
+        function(error) {
+          alert(error);
+        },
+        function() {}
+      );
+    }
+  }
 }
