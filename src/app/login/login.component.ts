c59071b4d1c3d29dd18d05322a5e277340d959da
User1 <user1@Skycliffitdomain.com>
2019-08-27 17:17:29 +0530

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+login: FormGroup;
+  constructor(private fb: FormBuilder, private udata: UserDataService, private router: Router) { }
+
+
+  ngOnInit() {this.login = this.fb.group({
+    user_email: new FormControl(),
+    user_password: new FormControl()
+  });
+  }
+  onLoginSubmit() {
+    this.udata.login(this.login.value.user_email,
+      this.login.value.user_password);
+    if (this.udata.redirectURL) {
+        this.router.navigateByUrl(this.udata.redirectURL);
+      } else {
+        this.router.navigate('/users');
+      }
+  }
+
+}
